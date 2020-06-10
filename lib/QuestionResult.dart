4ff5bc798549449bcfb0a88fe31f3d8d244c2449
Abdulaziz Alshabibi <aa353@evansville.edu>
2020-06-10 00:43:32 -0500

    fix identation in QuestionResult widget

--- lib/QuestionResult.dart
+++ lib/QuestionResult.dart
@@ -9,63 +9,63 @@ class QuestionResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-                width: double.infinity,
-                child: Card(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(8, 10, 8, 14),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: Text(question['question'], textAlign: TextAlign.left,),
-                        ),
-                        question['correct_answer'].toString() == question['user_answer'] ?
-                        Wrap(
-                          alignment: WrapAlignment.center,
-                          children: <Widget>[ 
-                            Padding(
-                              padding: const EdgeInsets.only(right: 7.0),
-                              child: Icon(
-                                Icons.thumb_up,
-                                color: Colors.green,
-                              ),
-                            ),
-                            Text(question['correct_answer'], 
-                              style: TextStyle(color: Colors.green),
-                              overflow: TextOverflow.fade),
-                          ],
-                        ) :
-                        Column(
-                          children: <Widget>[
-                            Wrap(
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.only(right: 7.0),
-                                  child: Icon(
-                                    Icons.thumb_down,
-                                    color: Colors.red,
-                                  ),
-                                ),
-                                Text(question['user_answer'],
-                                  style: TextStyle(color: Colors.red),
-                                  overflow: TextOverflow.fade,),
-                              ]
-                            ),
-                            Wrap(
-                              children: <Widget>[
-                                Text("correct answer: "),
-                                Text(question['correct_answer'],
-                                  style: TextStyle(color: Colors.green),
-                                  overflow: TextOverflow.fade
-                                )
-                              ]
-                            )
-                          ],
-                        )
-                      ]
+      width: double.infinity,
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(8, 10, 8, 14),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(question['question'], textAlign: TextAlign.left,),
+              ),
+              question['correct_answer'].toString() == question['user_answer'] ?
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: <Widget>[ 
+                  Padding(
+                    padding: const EdgeInsets.only(right: 7.0),
+                    child: Icon(
+                      Icons.thumb_up,
+                      color: Colors.green,
                     ),
                   ),
-                ),
-              );
+                  Text(question['correct_answer'], 
+                    style: TextStyle(color: Colors.green),
+                    overflow: TextOverflow.fade),
+                ],
+              ) :
+              Column(
+                children: <Widget>[
+                  Wrap(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right: 7.0),
+                        child: Icon(
+                          Icons.thumb_down,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(question['user_answer'],
+                        style: TextStyle(color: Colors.red),
+                        overflow: TextOverflow.fade,),
+                    ]
+                  ),
+                  Wrap(
+                    children: <Widget>[
+                      Text("correct answer: "),
+                      Text(question['correct_answer'],
+                        style: TextStyle(color: Colors.green),
+                        overflow: TextOverflow.fade
+                      )
+                    ]
+                  )
+                ],
+              )
+            ]
+          ),
+        ),
+      ),
+    );
   }
 }
